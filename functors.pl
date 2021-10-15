735131578c5980c7a21fc806cf9f5885db55848f
Richard Dalton <richard@devjoy.com>
2021-10-15 22:13:13 +0100

    Update functors

--- functors.pl
+++ functors.pl
@@ -1,7 +1,2 @@
-
-
-is_a(X) :- X = a.
-
-
-count_by([], 0).
-count_by([X|Xs], F, S) :- count_by(Xs, F, Z), call(is_a(X), S is 1 + Z.
+append([],L,L).
+append([H|T], L2, [H|L3]) :- append(T, L2, L3).
